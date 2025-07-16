876f44370f719341a303d91c32238266a2698230
Nicola-Diana Sincaru <Nicola-Diana.Sincaru@endava.com>
2025-07-16 16:44:56 +0300

    added automation run_project.bat

---
+++ run_project.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d %~dp0
+echo Activating virtual environment...
+call venv\Scripts\activate
+
+echo Running main.py...
+py src\main.py
+
+echo.
+echo Done. Press any key to close.
+pause
