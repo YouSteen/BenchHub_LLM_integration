f3fca75bfd73534c67a8dbe4061487c233528e43
Nicola-Diana Sincaru <Nicola-Diana.Sincaru@endava.com>
2025-07-18 11:16:56 +0300

    setup script

---
+++ scripts/setup.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Creating virtual environment...
+py -m venv venv
+
+echo Activating virtual environment...
+call venv\Scripts\activate
+
+echo Installing dependencies from requirements.txt...
+py -m pip install -r requirements.txt
+
+echo.
+echo ✅ Setup complete. Your virtual environment is ready.
+pause
